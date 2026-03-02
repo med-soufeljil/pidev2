2029233e7375b071f1eef6b45b67b5310c8203e9
msoufeljil66 <msoufeljil66@gmail.com>
2026-03-02 22:34:33 +0100

    Resolve merge conflicts and align dashboard/offre UI

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,32 +1,5 @@
 package test;
 
-<<<<<<< HEAD
-import entities.Apprenant;
-import entities.Formation;
-import entities.Niveau;
-import entities.Categorie;
-import services.ApprenantService;
-import services.FormationService;
-
-import java.sql.SQLException;
-import java.time.LocalDate;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        FormationService fs = new FormationService();
-
-       /* try {
-            fs.ajouter(new Formation(
-                    "Java JDBC",
-                    "Apprendre la connexion base de données",
-                    Niveau.INTERMEDIAIRE,
-                    30,
-                    Categorie.DEV,
-                    true
-            ));
-=======
 import services.CandidatService;
 import services.OffreService;
 import services.ReunionService;
@@ -35,128 +8,17 @@ import java.sql.SQLException;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        OffreService ps = new OffreService();
-
-        /*try {
-            ps.ajouter(new Offre("poste Ing","Senior","SQL,C++",1600));
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }*/
-
-       /* try {
-            ps.modifier(new Offre(2,"poste Ing","Senior","SQL,C++",1500));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-//
+        OffreService offreService = new OffreService();
+        CandidatService candidatService = new CandidatService();
+        ReunionService reunionService = new ReunionService();
 
         try {
-            System.out.println(ps.recuperer());
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-
-        CandidatService cs = new CandidatService();
-
-        /*try {
-            cs.ajouter(new Candidat("Ali", "Ben Salah", 12345678, 22113344, "Tunis", "ali@gmail.com", "cv.pdf"));
->>>>>>> feature-mohamed
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }*/
-
-<<<<<<< HEAD
-
-        /*try {
-            fs.modifier(new Formation(
-                    1,
-                    "Java JDBC Avancé",
-                    "JDBC + PreparedStatement",
-                    40,
-                    Categorie.DEV,
-                    Niveau.AVANCE,
-                    true
-            ));
-=======
-       /* try {
-            cs.modifier(new Candidat(2,"Ali",  12345678,"Ben Salah", 22113344, "Kelibia", "ali@gmail.com", "cv.pdf"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-//
-
-        try {
-            System.out.println(cs.recuperer());
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-
-        ReunionService rs = new ReunionService();
-        /*try {
-            rs.ajouter(new Reunion(1,2,LocalDateTime.of(2026, 2, 10, 10, 30), "https://meet.google.com/abc-defg-hij"));
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }*/
-
-        /*try {
-            rs.modifier(new Reunion(2, 1, 2, LocalDateTime.of(2026, 2, 10, 11, 0), "https://meet.google.com/updated-link"));
->>>>>>> feature-mohamed
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-
-        try {
-<<<<<<< HEAD
-            System.out.println(fs.recuperer());
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        ApprenantService as = new ApprenantService();
-        /*try {
-            as.ajouter(new Apprenant(
-                    "Ali",
-                    "Ben Salah",
-                    "ali@gmail.com",
-                    "Actif",
-                    LocalDate.of(2026, 2, 1),
-                    LocalDate.of(2026, 3, 1),
-                    1
-            ));
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }*/
-        try {
-            as.modifier(new Apprenant(
-                    2,
-                    "Mohamed",
-                    "Ben Salah",
-                    "ali@gmail.com",
-                    "Actif",
-                    LocalDate.of(2026, 2, 1),
-                    LocalDate.of(2026, 3, 1),
-                    1
-            ));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            System.out.println(as.recuperer());
+            System.out.println(offreService.recuperer());
+            System.out.println(candidatService.recuperer());
+            System.out.println(reunionService.recuperer());
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
     }
-
-
-
-=======
-            System.out.println(rs.recuperer());
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-
-
-    }
->>>>>>> feature-mohamed
 }
--- src/main/java/utils/MyDatabase.java
+++ src/main/java/utils/MyDatabase.java
@@ -3,16 +3,7 @@ package utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-<<<<<<< HEAD
 import java.sql.Statement;
-
-public class MyDatabase {
-    private final String USER = "root";
-    private final String PASSWORD = "";
-    private final String URL = "jdbc:mysql://localhost:3306/pidev";
-    public Connection connection;
-    public static MyDatabase instance = new MyDatabase();
-=======
 
 public class MyDatabase {
 
@@ -21,12 +12,10 @@ public class MyDatabase {
     private final String PASSWORD = "";
     private Connection connection;
     private static MyDatabase instance;
->>>>>>> feature-mohamed
 
     private MyDatabase() {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-<<<<<<< HEAD
             ensureSchema();
             System.out.println("Connected to database successfully");
         } catch (SQLException e) {
@@ -38,23 +27,12 @@ public class MyDatabase {
         if (instance == null) {
             instance = new MyDatabase();
         }
-=======
-            System.out.println("Connected");
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-    public static MyDatabase getInstance() {
-        if(instance == null)
-            instance = new MyDatabase();
->>>>>>> feature-mohamed
         return instance;
     }
 
     public Connection getConnection() {
         return connection;
     }
-<<<<<<< HEAD
 
     private void ensureSchema() {
         String create = "CREATE TABLE IF NOT EXISTS formation_feedback ("
@@ -77,6 +55,4 @@ public class MyDatabase {
             // compatibility with existing schema/user rights
         }
     }
-=======
->>>>>>> feature-mohamed
 }
